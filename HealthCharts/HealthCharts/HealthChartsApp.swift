d298bc6fc1210a6576c1ac2c123fd1d6901d85c1
bulat <its.bulat@icloud.com>
2025-01-01 22:40:42 +0300

    Initial Xcode project

---
+++ HealthCharts/HealthCharts/HealthChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthChartsApp.swift
+//  HealthCharts
+//
+//  Created by Булат Камалетдинов on 01.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct HealthChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
